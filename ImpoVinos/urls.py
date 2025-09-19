056845a501744b44da05a87774294dc51f2a180a
Ignacio Millan <igna.millan@duocuc.cl>
2025-09-20 05:35:21 +1200

    Modificados los templates con la estructura de Django y poder interactuar con los estilos. Organizados en carpetas para mejor organizacion y facilitar el acceso a futuro.

---
+++ ImpoVinos/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Para la landing page
+    path("", views.index, name="index"),
+
+    # Interacciones (se puede cambiar este nombre)
+    path("interacciones/comprar/", views.comprar, name="comprar"),
+    path("interacciones/nosotros/", views.nosotros, name="nosotros"),
+    path("interacciones/contacto/", views.contacto, name="contacto"),
+    path("interacciones/carrito/", views.carrito, name="carrito"),
+    path("interacciones/Inventario/", views.Inventario, name="Inventario"),
+
+    # Productos
+    path("productos/nacionales/", views.prod_nacionales, name="nacionales"),
+    path("productos/importados/", views.prod_importados, name="importados"),
+
+    # Usuarios
+    path("usuarios/ingresa/", views.ingresa, name="ingresa"),
+    path("usuarios/registro/", views.registro, name="registro"),
+    path("usuarios/recuperar/", views.recuperar, name="recuperar"),
+    path("usuarios/perfil/", views.perfil, name="perfil"),
+    path("usuarios/modificar/", views.modificarPerfil, name="modificarPerfil"),
+]
